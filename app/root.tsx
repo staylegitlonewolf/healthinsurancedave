088fff5d99f29d9e917edf2c3e5c5dd7364345cd
staylegitlonewolf <staylegitdev@gmail.com>
2025-08-26 12:04:51 -0400

    FINAL CACHE BUST - Added timestamp and cache-bust file to force GitHub Pages deployment

--- app/root.tsx
+++ app/root.tsx
@@ -46,6 +46,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta http-equiv="Pragma" content="no-cache" />
         <meta http-equiv="Expires" content="0" />
+        <meta name="deployment-timestamp" content="2024-12-19-15-30-00" />
         <link rel="icon" type="image/png" href="/healthinsurancedave/logo.png" />
         <link rel="shortcut icon" type="image/png" href="/healthinsurancedave/logo.png" />
         <link rel="apple-touch-icon" href="/healthinsurancedave/logo.png" />
